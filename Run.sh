312e45f1a1eb73aa432fdb9d1b4612d27f37c1b0
AlexSid <seedteam@yandex.ru>
2020-08-13 18:37:00 +0300

    start commit

--- Run.sh
+++ Run.sh
@@ -16,9 +16,9 @@ PORTHTTPS=`cat "$DIRECTORY/https-port.txt"`
 echo $DIRECTORY
 echo $PORTHTTP
 echo $NAME
-docker run -d -p $PORTHTTP:80 -p $PORTHTTPS:443 -p $PORTSSH:22 -p $PORTDB:3306 --name $NAME -v $DIRECTORY/lamp-mariadb:/var/lib/mysql -v $DIRECTORY/lamp-docroot:/var/www/html -v $DIRECTORY/lamp-apache-conf:/etc/apache2 -v $DIRECTORY/lamp-mariadb-conf:/etc/mysql palachorel/lamp-ubuntu18.04
+docker run -d -it -p $PORTHTTP:80 -p $PORTHTTPS:443 -p $PORTSSH:22 -p $PORTDB:3306 --name $NAME -v $DIRECTORY/lamp-mariadb:/var/lib/mysql -v $DIRECTORY/lamp-docroot:/var/www/html -v $DIRECTORY/lamp-apache-conf:/etc/apache2 -v $DIRECTORY/lamp-mariadb-conf:/etc/mysql  palachorel/lamp-ubuntu18.04 /bin/bash -c "service apache2 start && service cron start && service mariadb start && /bin/bash"
 
 
 echo "docker runing $NAME PORTHTTP: $PORTHTTP"
 echo "docker exec -it $NAME bash"
-sleep 10
+sleep 10000
