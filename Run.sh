1ee8041b9781ac80138de331c23c1d6b0ffb69bc
Your Name <you@example.com>
2020-11-23 12:09:10 +0300

    deleted https port

--- Run.sh
+++ Run.sh
@@ -10,13 +10,13 @@ NAME=${PWD##*/}
 PORTHTTP=`cat "$DIRECTORY/http-port.txt"`
 PORTSSH=`cat "$DIRECTORY/ssh-port.txt"`
 PORTDB=`cat "$DIRECTORY/db-port.txt"`
-PORTHTTPS=`cat "$DIRECTORY/https-port.txt"`
+
 
 
 echo $DIRECTORY
 echo $PORTHTTP
 echo $NAME
-docker run -d -it -p $PORTHTTP:80 -p $PORTHTTPS:443 -p $PORTSSH:22 -p $PORTDB:3306 --name $NAME -v $DIRECTORY/lamp-mariadb:/var/lib/mysql -v $DIRECTORY/lamp-docroot:/var/www/html -v $DIRECTORY/lamp-apache-conf:/etc/apache2 -v $DIRECTORY/lamp-mariadb-conf:/etc/mysql  palachorel/lamp-ubuntu18.04 /bin/bash -c "service apache2 start && service cron start && service mariadb start && /bin/bash"
+docker run -d -it -p $PORTHTTP:80 -p $PORTSSH:22 -p $PORTDB:3306 --name $NAME -v $DIRECTORY/lamp-mariadb:/var/lib/mysql -v $DIRECTORY/lamp-docroot:/var/www/html -v $DIRECTORY/lamp-apache-conf:/etc/apache2 -v $DIRECTORY/lamp-mariadb-conf:/etc/mysql  palachorel/lamp-ubuntu18.04 /bin/bash -c "service apache2 start && service cron start && service mariadb start && /bin/bash"
 
 
 echo "docker runing $NAME PORTHTTP: $PORTHTTP"
