377035e25700f36211ee4b0d95d70fa3ffdd41ef
AlexSid <palach.orl@mail.ru>
2021-01-15 12:26:00 +0300

    обновлен php до 7.4

--- Run.sh
+++ Run.sh
@@ -16,7 +16,7 @@ PORTDB=`cat "$DIRECTORY/db-port.txt"`
 echo $DIRECTORY
 echo $PORTHTTP
 echo $NAME
-docker run -d -it -p $PORTHTTP:80 -p $PORTSSH:22 -p $PORTDB:3306 --name $NAME -v $DIRECTORY/lamp-mariadb:/var/lib/mysql -v $DIRECTORY/lamp-docroot:/var/www/html -v $DIRECTORY/lamp-apache-conf:/etc/apache2 -v $DIRECTORY/lamp-mariadb-conf:/etc/mysql  palachorel/lamp7.4 /bin/bash -c "service apache2 start && service cron start && service mariadb start && /bin/bash"
+docker run -d -it -p $PORTHTTP:80 -p $PORTSSH:22 -p $PORTDB:3306 --name $NAME -v $DIRECTORY/lamp-mariadb:/var/lib/mysql -v $DIRECTORY/lamp-docroot:/var/www/html -v $DIRECTORY/lamp-apache-conf:/etc/apache2 -v $DIRECTORY/lamp-mariadb-conf:/etc/mysql  palachorel/lamp7.4_git_cron_ssh_composer /bin/bash -c "service apache2 start && service cron start && service mariadb start && /bin/bash"
 
 
 echo "docker runing $NAME PORTHTTP: $PORTHTTP"
